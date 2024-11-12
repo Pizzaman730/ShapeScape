454704124ce9ea83e6f95c16ee0dfbbef3c14131
Abstractmelon <abstractmelons@gmail.com>
2024-11-11 17:12:09 -0700

    base

--- obj/Debug/net8.0/SquarePlatformer.AssemblyInfo.cs
+++ obj/Debug/net8.0/SquarePlatformer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SquarePlatformer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+148c460ef6b791273d8efacf455870ad0fba4218")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c7580bfc2618a51bed0a10ab2af8d5f10d30e80")]
 [assembly: System.Reflection.AssemblyProductAttribute("SquarePlatformer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SquarePlatformer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
